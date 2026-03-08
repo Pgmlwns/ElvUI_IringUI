e3e2ff5ad13e81a696a35702e9510daaf43f8c6e
Iring <emulcube@gmail.com>
2026-03-08 22:35:55 +0900

    Update Core.lua

--- Config/Core.lua
+++ Config/Core.lua
@@ -1,6 +1,7 @@
-local IR, F, E, L, V, P, G = unpack(select(2, ...))
+local E, L, V, P, G = unpack(select(2, ...))
+local IR = E:GetModule('IringUI') -- IR 객체는 여기서 가져옵니다.
 
--- [표준 방식] 기존 설정을 보존하며 모든 테이블 구조를 강제 생성
+-- [핵심] 기존 설정을 보존하며 모든 테이블 구조를 강제 생성
 P["IringUI"] = P["IringUI"] or {}
 P["IringUI"]["skin"] = P["IringUI"]["skin"] or { ["enable"] = true, ["stripes"] = true, ["shadow"] = true }
 P["IringUI"]["layout"] = P["IringUI"]["layout"] or { ["topBar"] = true, ["bottomBar"] = true }
